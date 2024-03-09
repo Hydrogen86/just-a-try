028906d54331acb0216c95ec1a5fb24aa8da0e69
MagnoRobert <magnorobert86@gmail.com>
2024-03-09 19:19:48 +0800

    Comitted by Hydrogen86

---
+++ Magno-Robert-Php-file/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>index</title>
+</head>
+<body>
+
+    <div class="wrapper">
+        <h1>Welcome to Pinoy Song Book</h1>
+        <h3>Song List:</h3>
+        <ol>
+            <li><a href="song1.php">Demonyo</a></li>
+            <li><a href="song2.php">Eroplanong Papel</a></li>
+            <li><a href="song3.php">Ikaw Lamang</a></li>
+            <li><a href="song4.php">Pagbilang ng Tatlo</a></li>
+            <li><a href="song5.php">Pansamantala</a></li>
+	    <li><a href="#">New Song to be Added</a></li>
+        </ol>
+    </div>
+
+</body>
+</html>
