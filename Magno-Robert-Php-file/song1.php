9e2c958d6b8ae66e35c37c2ca5210e07e9e78f9f
MagnoRobert <magnorobert86@gmail.com>
2024-03-09 19:22:18 +0800

    Hydrogen86

---
+++ Magno-Robert-Php-file/song1.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylingForSongs.css">
+    <title>Demonyo</title>
+</head>
+<body>
+    
+    <div class="wrapper">
+        <a href="index.php">Back</a>
+        <h2>Demonyo</h2>
+        <h4>(Artist: Juan Karlos Labajo)</h4>
+        <?php
+            echo" [Intro:]&nbsp;&nbsp;C,  E,  Am,  F,  x2 <br><br>";
+            echo" [Verse 1:]&nbsp;&nbsp;&nbsp; C,  E,  Am,  F <br>";
+            echo" Alam mo ba na hindi kita magugustuhan?<br>";
+            echo" Kung pangit ang ugali mo<br>";
+            echo" Kaya sinta, sana ay huwag ka nang magtaka<br>";
+            echo" Kung ba't napa-ibig sayo<br> <br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;&nbsp; F,  G,  C,  F <br>";
+            echo" Ikaw ay anghel na napadpad sa impyerno<br>";
+            echo" Ako ang demonyo gagabay sa iyo<br>";
+            echo" Pabalik sa langit   habang tayo<br>";
+            echo" Ay paakyat ako'y napa-ibig   sayo<br><br>";
+
+            echo" [Instrumental]&nbsp;&nbsp; C, E, Am, F  x2<br><br>";
+
+            echo" [Verse 2]&nbsp;&nbsp; C, E, Am, F<br>";
+            echo" Pano na pagbigla kang wala sa aking piling<br>";
+            echo" Kung ikaw ang nagbibigay ng kulay sa king<br>";
+            echo" Puso at damdamin<br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;&nbsp; F,  G,  C,  F <br>";
+            echo" Ikaw ay prinsesa napadpad sa malayo<br>";
+            echo" Ako ang aliping gagabay sa iyo<br>";
+            echo" Pabalik sa palasyo<br>";
+            echo" Habang tayo'y naglalakbay<br>";
+            echo" Ako'y nahulog ... sayo<br><br>";
+
+            echo" [Instrumental]&nbsp;&nbsp; C, E, Am, F  x2<br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;&nbsp; F,  G,  C,  F <br>";
+            echo" Ikaw ay anghel na napadpad sa impyerno<br>";
+            echo" Ako ang demonyo gagabay sa iyo<br>";
+            echo" Pabalik sa langit   habang tayo<br>";
+            echo" Ay paakyat ako'y napa-ibig   sayo<br><br>";
+
+            echo" [Outro:]&nbsp;&nbsp; C, E, Am, F<br><br>";
+            echo" [-----End Here----]<br>";
+        ?>
+    </div>
+</body>
+</html>
