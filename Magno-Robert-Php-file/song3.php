4afa46c1bce23bdbc819ddd3d1bc4e5b8afac5fe
MagnoRobert <magnorobert86@gmail.com>
2024-03-09 19:23:57 +0800

    Comitted by Hydrogen86 for song3

---
+++ Magno-Robert-Php-file/song3.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylingForSongs.css">
+    <title>Ikaw Lamang</title>
+</head>
+<body>
+    <div class="wrapper">
+        <a href="index.php">Back</a>
+        <h2>Ikaw Lamang</h2>
+        <h4>(Artist: Silent Sanctuary)</h4>
+        <?php
+            echo" [Intro]&nbsp;&nbsp;A - F#m<br><br>";
+
+            echo" [Verse 1:]&nbsp;&nbsp;A, F#m<br>";
+            echo" Di ko maintindihan<br>";
+            echo" Ang nilalaman ng puso<br>";
+            echo" Tuwing magkahawak ang ating kamay<br>";
+            echo" Pinapanalangin lagi tayong magkasama<br>";
+            echo" Hinihiling bawat oras kapiling ka<br><br>";
+
+            echo" [Pre-Chorus:]&nbsp;&nbsp;Dm7, A9, F#m, F, E<br>";
+            echo" Sa lahat ng aking ginagawa<br>";
+            echo" Ikaw lamang ang nasa isip ko sinta<br>";
+            echo" Sana’y di na tayo magkahiwalay<br>";
+            echo" Kahit kailan pa man<br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;A, F#m, A, F#m, D9, D<br>";
+            echo" Ikaw lamang ang aking minamahal<br>";
+            echo" Ikaw lamang ang tangi kong inaasam<br>";
+            echo" Makapiling ka habang buhay<br>";
+            echo" Ikaw lamang sinta<br>";
+            echo" Wala na kong hihingin pa<br>";
+            echo" Wala na..huhhhohhhh<br><br>";
+
+            echo" [Verse 2:]&nbsp;&nbsp;A, F#m<br>";
+            echo" Ayoko ng maulit pa<br>";
+            echo" Ang nakaraang ayokong maalala<br>";
+            echo" Bawat oras na wala ka<br>";
+            echo" Parang mabigat na parusa<br>";
+            echo" Huwag mong kakalimutan na kahit nag-iba<br>";
+            echo" Hindi ako tumigil magmahal sayo sinta<br><br>";
+
+            echo" [Pre-Chorus:]&nbsp;&nbsp;Dm7, A9, F#m, F, E<br>";
+            echo" Sa lahat ng aking ginagawa<br>";
+            echo" Ikaw lamang ang nasa isip ko sinta<br>";
+            echo" Sana’y di na tayo magkahiwalay<br>";
+            echo" Kahit kailan pa man<br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;A, F#m, A, F#m, D9, D<br>";
+            echo" Ikaw lamang ang aking minamahal<br>";
+            echo" Ikaw lamang ang tangi kong inaasam<br>";
+            echo" Makapiling ka habang buhay<br>";
+            echo" Ikaw lamang sinta<br>";
+            echo" Wala na kong hihingin pa<br>";
+            echo" Wala na..huhhhohhhh<br><br>";
+
+            echo" [Instrumental:]&nbsp;&nbsp;F, E x4 <br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;A, F#m, A, F#m, D9, D<br>";
+            echo" Ikaw lamang ang aking minamahal<br>";
+            echo" Ikaw lamang ang tangi kong inaasam<br>";
+            echo" Makapiling ka habang buhay<br>";
+            echo" Ikaw lamang sinta<br>";
+            echo" Wala na kong hihingin pa<br>";
+            echo" Wala na..huhhhohhhh<br><br>";
+            
+            echo" [-----End Here-----]<br>";
+        ?>
+    </div>
+</body>
+</html>
