e36e0fb618b76d6215fa446c5c5c97a1782c4e69
MagnoRobert <magnorobert86@gmail.com>
2024-03-09 19:23:05 +0800

    Committed by Hydrogen86

---
+++ Magno-Robert-Php-file/song2.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylingForSongs.css">
+    <title>Eroplanong Papel</title>
+</head>
+<body>
+    <div class="wrapper">
+        <a href="index.php">Back</a>
+        <h2>Eroplanong Papel</h2>
+        <h4>(Artist: December Avenue)</h4>
+        <?php
+            echo" [Intro]&nbsp;&nbsp;C CM7 (3x), Am7, Am9, C, CM7, Am7, Am9<br><br>";
+            echo" [Verse 1:]&nbsp;&nbsp;C, Cm7, Am7, Am9, Fm7<br>";
+            echo" Sandali, 'wag kang mapupuno sa paghihirap <br>";
+            echo" Darating din ang pag-asa mong pinapangarap<br>";
+            echo" 'Di ba sapat na ika'y mayro'ng pag-ibig<br>";
+            echo" 'Ka'y ngumiti<br><br>";
+
+            echo" [Verse 2:]&nbsp;&nbsp;C, Cm7, Am7, Am9, Fm7<br>";
+            echo" Ihip ng hangin sa kamay mong malamig<br>";
+            echo" Daing na tinig, nasa aking pandinig<br>";
+            echo" Liliparin ang isipan mo't damdamin<br>";
+            echo" Makarating pa kaya sa kanyang piling?<br>";
+            echo" 'Ka'y pumikit (E-single strum)<br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;C, Cm7, Am7, Am9, F, E<br>";
+            echo" Kung panalangin ko'y 'di marinig<br>";
+            echo" Abutin man ng bawat sandali<br>";
+            echo" Kailangan kong isigaw, ako'y iyong iyo<br>";
+            echo" Ang dalangin ng puso'y ikaw<br><br>";
+
+            echo" [Verse 3:]&nbsp;&nbsp;C, Cm7, Am7, Am9, Fm7<br>";
+            echo" Sandali, 'wag mong pigilan ang iyong pagluha<br>";
+            echo" Damdamin mo'y aahon sa tumigil na tadhana<br>";
+            echo" Aabutin ng 'yong palad ang hangarin<br>";
+            echo" Makarating pa kaya sa kanyang piling?<br>";
+            echo" Ika'y pumikit<br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;C, Cm7, Am7, Am9, F, E<br>";
+            echo" Kung panalangin ko'y 'di marinig<br>";
+            echo" Abutin man ng bawat sandali<br>";
+            echo" Kailangan kong isigaw, ako'y iyong iyo<br>";
+            echo" Ang dalangin ng puso'y ikaw<br><br>";
+
+            echo" [Adlib:]&nbsp;&nbsp;C, CM7, Am7, Am9, F, E<br><br>";
+
+            echo" [Bridge:]&nbsp;&nbsp;F, G, C, C/B, Am7, F, E <br>";
+            echo" Ang kung sa bawat higpit ng aking pagdaramdam<br>";
+            echo" Ay hindi ka malapitan<br>";
+            echo" Nakikiusap na lang<br><br>";
+
+            echo" [Chorus:]&nbsp;&nbsp;C, Cm7, Am7, Am9, F, E<br>";
+            echo" Kung panalangin ko'y 'di marinig<br>";
+            echo" Abutin man ng bawat sandali<br>";
+            echo" Kailangan kong isigaw, ako'y iyong iyo<br>";
+            echo" Ang dalangin ng puso'y ikaw<br><br>";
+
+            echo" [Interlude:]&nbsp;&nbsp;C, CM7, Am7, Am9, F<br><br>";
+
+            echo" (E - single strum)<br>";
+            echo" Ang dalangin ng puso'y ikaw<br>";
+            echo" [Outro:]&nbsp;&nbsp;C<br>";
+            echo" Ang panalangin sana'y marinig<br><br>";
+
+            echo" [-----End Here-----]<br>";
+        ?>
+    </div>
+</body>
+</html>
