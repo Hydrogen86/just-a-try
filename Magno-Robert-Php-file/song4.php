c2d6bfabd9743e8b671e698cf7e6728b19a040c0
MagnoRobert <magnorobert86@gmail.com>
2024-03-09 19:24:47 +0800

    committed by Hydrogen86 for song 4

---
+++ Magno-Robert-Php-file/song4.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylingForSongs.css">
+    <title>Pagbilang ng Tatlo</title>
+</head>
+<body>
+    <div class="wrapper">
+            <a href="index.php">Back</a>
+            <h2>Pagbilang ng Tatlo</h2>
+            <h4>(Artist: Bandang Lapis)</h4>
+            <?php
+                echo" [Intro:]&nbsp;&nbsp;&nbsp;G, Cadd9 x2<br><br>";
+
+                echo" [Verse 1:]&nbsp;&nbsp;&nbsp;G, Cadd9, Em7, D, Cadd9<br>";
+                echo" Huling bigkas ng iyong mga sinabi<br>";
+                echo" Ang sabi mo ayaw mo na at tama na<br>";
+                echo" Wala akong magagawa kundi lumuha<br>";
+                echo" At tanggapin ang lahat ng katotohanan na wala ka na(D-Dsus4-D)<br><br>";
+
+                echo" [Chorus]&nbsp;&nbsp;&nbsp;G, Cadd9 x2, Em7, D, Cadd9, G<br>";
+                echo" Isisigaw ko na lang sa langit<br>";
+                echo" Iiiyak ko na lang sa tanawin<br>";
+                echo" At pagkabilang kong tatlo<br>";
+                echo" Kakalimutan na kita<br><br>";
+
+                echo" [Instrumental:]&nbsp;&nbsp;&nbsp;G, Cadd9 x2<br><br>";
+
+                echo" [Verse 2:]&nbsp;&nbsp;&nbsp;G, Cadd9, Em7, D, Cadd9<br>";
+                echo" Kailangan ko nang tanggapin ang lahat<br>";
+                echo" Na wala nang ikaw at ako na namamagitan<br>";
+                echo" Isang alaala na lang ba ang nakaraan<br>";
+                echo" Na dating kay saya ngayon ay kay lungkot na nag-iisa(D-Dsus4-D)<br><br>";
+
+                echo" [Chorus]&nbsp;&nbsp;&nbsp;G, Cadd9 x2, Em7, D, Cadd9, G<br>";
+                echo" Isisigaw ko na lang sa langit<br>";
+                echo" Iiiyak ko na lang sa tanawin<br>";
+                echo" At pagkabilang kong tatlo<br>";
+                echo" Kakalimutan na kita<br><br>";
+
+                echo" [Bridge:]&nbsp;&nbsp;&nbsp;Cadd9, D, Em7, D<br>";
+                echo" Pipilitin kong kayanin<br>";
+                echo" Kung ito na ang huling yugto<br>";
+                echo" Oras na rin para ba ihinto<br><br>";
+
+                echo" [Outro:]&nbsp;&nbsp;&nbsp;Cadd9, D x2<br>";
+                echo" Kakalimutan na kita<br>";
+                echo" Salamat nga pala sa lahat ng 'yong ginawa<br><br>";
+                echo" [End:]&nbsp;&nbsp;&nbsp;G, Cadd9 x2<br><br>";
+
+                echo" [-----End Here----]<br>";
+            ?>
+    </div>
+</body>
+</html>
