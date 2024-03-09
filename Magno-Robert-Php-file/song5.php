48d0fb7669a16997ae2ceb15fc102cf741bce867
MagnoRobert <magnorobert86@gmail.com>
2024-03-09 19:25:28 +0800

    committed by Hydrogen86 for song 5

---
+++ Magno-Robert-Php-file/song5.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylingForSongs.css">
+    <title>Pansamantala</title>
+</head>
+<body>
+    <div class="wrapper">
+            <a href="index.php">Back</a>
+            <h2>Pansamantala</h2>
+            <h4>(Artist: Callalily)</h4>
+            <?php
+                echo" [Intro:]&nbsp;&nbsp;&nbsp;G, G/F#, Em7, Cadd9<br><br>";
+
+                echo" [Verse 1:]&nbsp;&nbsp;&nbsp;G, G/F#, Em7, Cadd9<br>";
+                echo" Siya na ang mayaman<br>";
+                echo" Siya na ang may oto, siya na<br>";
+                echo" Siya na ang meron ng lahat<br>";
+                echo" Ng bagay na wala ako<br><br>";
+
+                echo" [Am, G/B, Cadd9, D, Dsus4]<br>";
+                echo" Kapag nalagay ka sa alanganin<br>";
+                echo" Heto na naman tayo<br><br>";
+
+                echo" [Chorus:]&nbsp;&nbsp;&nbsp;G, G/F#, Em7, Cadd9<br>";
+                echo" Pansamantalang unan<br>";
+                echo" Sa tuwing ika'y nahihirapan<br>";
+                echo" Pansamantalang panyo <br>";
+                echo" Sa tuwing ika'y nasasaktan<br><br>";
+
+                echo" [Verse 2:]&nbsp;&nbsp;&nbsp;G, G/F#, Em7, Cadd9<br>";
+                echo" Bakit ba sa akin na lang palagi ang takbo<br>";
+                echo" Sa tuwing kayo ay may away<br>";
+                echo" Ako ang lagi mong karamay<br>";
+                echo" Di naman tayo, hindi<br>";
+                echo" Diba't hindi(D - strum once)<br><br>";
+
+                echo" [Chorus:]&nbsp;&nbsp;&nbsp;G, G/F#, Em7, Cadd9<br>";
+                echo" Pansamantalang unan<br>";
+                echo" Sa tuwing ika'y nahihirapan<br>";
+                echo" Pansamantalang panyo <br>";
+                echo" Sa tuwing ika'y nasasaktan<br><br>";
+
+                echo" [Bridge:]&nbsp;&nbsp;&nbsp;Em7, Cadd9 x3 & D Dsus4<br>";
+                echo" Kaibigan lang bang maituturing<br>";
+                echo" Ang hirap naman yatang mangapa sa dilim<br>";
+                echo" Sino nga ba talaga sa amin ang iyong<br><br>";
+
+                echo" [Chorus:]&nbsp;&nbsp;&nbsp;G, G/F#, Em7, Cadd9<br>";
+                echo" Pansamantalang unan<br>";
+                echo" Sa tuwing ika'y nahihirapan<br>";
+                echo" Pansamantalang panyo <br>";
+                echo" Sa tuwing ika'y nasasaktan<br><br>";
+
+                echo" [Outro:]&nbsp;&nbsp;&nbsp;G, G/F#, Em7, Cadd9<br>";
+                echo" Pansamantala<br>";
+                echo" Pansamantala<br>";
+                echo" Pansamantala<br>";
+                echo" Tanggap ko na<br><br>";
+
+                echo" [End:] G - single strum<br><br>";
+
+                echo" [-----End Here----]<br>";
+            ?>
+    </div>
+</body>
+</html>
