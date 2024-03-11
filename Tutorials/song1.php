49bbd18589ffbdd3cc1f348feae14903ab6be77a
MagnoRobert <magnorobert86@gmail.com>
2024-03-11 20:23:39 +0800

    another try

---
+++ Tutorials/song1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jopay</title>
+</head>
+<body>
+    <h2>Jopay</h2>
+    <h4>Artist: Mayonnaise</h4>
+
+    <?php
+        echo"Lyrics: <br><br>";
+        echo" Jopaay Kamusta ka na?<br>";
+        echo" Palagi kita'ng pinapanood nakikita<br>";
+        echo" <br>";
+        echo" <br>";
+        echo" <br>";
+        echo" <br>";
+    ?>
+
+    <button><a href="index.php">Back</a></button>
+    
+
+</body>
+
+    <style>
+        body{
+            text-align: center;
+        }
+        button a {
+            text-decoration: none;
+        }
+    </style>
+
+</html>
